044c010001ed8231b34b4459f19aacfe26ba96ac
dhruv-2000-11 <85791432+dhruv-2000-11@users.noreply.github.com>
2022-02-18 10:58:23 +0530

    Add files via upload

---
+++ prob1.c
@@ -0,0 +1,22 @@
+/*
+------------------------------------------------------------
+# Written in Sublime Text
+# Author  : Dhruv N.Devaliya                                                                  
+# Code    :                                                 
+# Date    :                                                 
+# Use     :                                                                                                           
+# you can modify this code as per your need                 
+------------------------------------------------------------
+*/
+main()
+{
+    int sub[50],i;
+    for(i=0;i<=48;i++);
+    {
+        sub[i]=i;
+        printf("\n%d",sub[i]);
+    }
+
+}
+
+// Here output of the code is = 49
---
+++ prob2.c
@@ -0,0 +1,28 @@
+/*
+------------------------------------------------------------
+# Written in Sublime Text
+# Author  : Dhruv N.Devaliya                                                                  
+# Code    :                                                 
+# Date    :                                                 
+# Use     :                                                                                                           
+# you can modify this code as per your need                 
+------------------------------------------------------------
+*/
+main()
+{
+    char c[2]="A";
+    printf("\n%c",c[0]);
+    printf("\n%s",c);
+    
+}
+
+/* output of the code is 
+
+D:\Dhruv\c programing\assignment2>prob2
+
+A
+A
+D:\Dhruv\c programing\assignment2>     
+
+
+*/
